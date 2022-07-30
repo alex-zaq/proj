e78c60f51f49e0effbd4098d2f64250549c22884
alex <example@bk.ru>
2022-07-30 21:11:18 +0300

    filmRates

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
 "use strict";
 
+// let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
 
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
+// for (let i = 0; i < 2; i++) {
 
-const func1 = function(a,b){
-  console.log(a,b);
+//   let lastWatchedFilmName = prompt("Один из последних просмотренных фильмов?",);
+//   let lastWatchedFilmRate = prompt("Насколько оцените его?", "0");
+
+//   personalMovieDB.movies[lastWatchedFilmName] = lastWatchedFilmRate;
+
+// }
+
+// console.log(personalMovieDB);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
+const aa = prompt('Один из последних просмотренных фильмов?', ''),
+      bb = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+personalMovieDB.movies.aa = bb;
+personalMovieDB.movies.c = d;
 
 
-const a = 3;
-
-
-
-
-
-
-
+console.log(personalMovieDB.movies);
 
